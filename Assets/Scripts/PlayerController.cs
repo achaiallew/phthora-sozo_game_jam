f4faf6d057f7eeeef489ee910da3abb7431dc9e3
Achaia Beukes <achaiallew@gmail.com>
2026-06-30 20:45:38 +0200

    Fixed Player Control; Added Assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
@@ -12,9 +11,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     private InputSystem_Actions controls;
     private Vector2 moveInput;
+    private Vector2 turnInput;
+    public float mouseSens = 0.3f;
 
     public float jumpForce = 500;
     private bool isGround = true;
+
+    private Animator playerAnim;
 
 
     void Awake()
@@ -23,6 +26,8 @@ public class PlayerController : MonoBehaviour
         playerRB = GetComponent<Rigidbody>();
 
         controls = new InputSystem_Actions();
+
+        playerAnim = GetComponent<Animator>();
 
     }
 
@@ -35,20 +40,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveInput = controls.Player.Move.ReadValue<Vector2>();
+        turnInput = controls.Player.Look.ReadValue<Vector2>();
 
-
-        Vector3 move = transform.right * moveInput.y +
-                        transform.forward * -moveInput.x;
+        transform.Rotate(turnInput.x * transform.up*mouseSens);
         
-        transform.Translate(move * playerSpeed * Time.deltaTime);
-        //Debug.Log(moveInput);
+        if (controls.Player.Move.IsInProgress())
+        {
+            //Debug.Log("Walk");
+            playerAnim.SetBool("walkHold", true);
+        } else {    playerAnim.SetBool("walkHold", false);  }
   
 
         if (controls.Player.Jump.triggered && isGround)
         {
             Debug.Log("Jump");
-            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
             isGround = false;
+            playerAnim.SetTrigger("jumpTrig");
+            //playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
 
         if (controls.Player.Crouch.IsPressed())
@@ -60,10 +69,24 @@ public class PlayerController : MonoBehaviour
 
         if (controls.Player.Sprint.IsPressed())
         {
-            Debug.Log("Sprint");             
-        } else if (controls.Player.Sprint.WasReleasedThisFrame()){
-            Debug.Log("Unsprint");         
+            //Debug.Log("Sprint");  
+            playerAnim.SetBool("sprintHold", true);           
+        } else if (controls.Player.Sprint.WasReleasedThisFrame()){ 
+            //Debug.Log("Unsprint");      
+            playerAnim.SetBool("sprintHold", false);    
         }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = transform.right * moveInput.x +
+                        transform.forward * moveInput.y;
+        
+        //transform.Translate(move * playerSpeed * Time.deltaTime);
+        //Debug.Log(moveInput);
+        playerRB.linearVelocity = new Vector3(move.x * playerSpeed, playerRB.linearVelocity.y,move.z * playerSpeed);
+        
+        
     }
 
     void OnCollisionEnter(Collision collision)
@@ -85,6 +108,6 @@ public class PlayerController : MonoBehaviour
     //     Vector3 move = transform.right * moveInput.x +
     //                     transform.forward * moveInput.y;
 
-    //     controller.Move(move * 5f * Time.deltaTime);
-    // }
+    //     controller.Move(move * playerSpeed * Time.deltaTime);
+    // } 
 }
